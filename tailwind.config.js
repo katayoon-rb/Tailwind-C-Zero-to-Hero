8d7ecacdc4c2d43f4d4edef30bd5e4825ca8c6ee
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-20 16:36:19 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": { transform: "scaleY(0.5)" },
+          "50%": { transform: "scaleY(1.5)" }
+        }
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      }
+    }
+  },
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+    require("./plugins/tableCaption"),
+    require("./plugins/buttonPlugin"),
+    require("./plugins/outlineSolid")
+  ]
+}
